4c0928d0254049a798d3e988d76098b3d87e93b8
fini <fini@kakao.com>
2021-01-04 12:18:15 +0900

    Change graph structure from list to linked list

--- src/waffle_topology/waffle_topology/extract_topology.py
+++ src/waffle_topology/waffle_topology/extract_topology.py
@@ -2,7 +2,6 @@
 
 import rclpy
 from rclpy.node import Node
-
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import Odometry
@@ -10,7 +9,6 @@ from geometry_msgs.msg import Point
 from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import Marker
 
-import numpy as np
 import math
 
 from waffle_topology.graph import Graph
@@ -55,6 +53,12 @@ class Extract_topology(Node):
         # self.history.colors.append(color)
         self.history_publisher = self.create_publisher(Marker, '/topology/history', 10)
     
+    def init_colors(self):
+        self.COLOR_NODE = ColorRGBA(r=0.53, g=0.9, b=0.5, a=0.2)        # Green
+        self.COLOR_EDGE = ColorRGBA(r=0.53, g=0.9, b=0.5, a=0.2)        # Green
+        self.COLOR_INTERSECTION = ColorRGBA(r=1., g=0.73, b=0., a=0.2)  # Yellow
+        self.COLOR_LEAF = ColorRGBA(r=0.95, g=0.37, b=0.37, a=0.2)      # Red
+
     def area_callback(self, msg):
         # Generate graph from gridmap message
         try:
@@ -64,22 +68,22 @@ class Extract_topology(Node):
             return
 
         # Search next goal
-        
-        
-
-
-        
 
         # Color important elements
-        topology.update_node_color_alpha(topology.root[1], 1.)
-        topology.update_edge_color_alpha(topology.root, 1.)
+        # topology.update_node_color_alpha(topology.root[1], 1.)
+        # topology.update_edge_color_alpha(topology.root, 1.)
 
         # Publish results
-        self.nodes_publisher.publish(topology.nodes_marker)
-        self.edges_publisher.publish(topology.edges_marker)
+        # self.nodes_publisher.publish(topology.nodes_marker)
+        # self.edges_publisher.publish(topology.edges_marker)
 
         # Log
         # self.draw_history(topology)
+
+        # Visualize graph
+        # self.init_colors()
+        # self.nodes_marker, self.edges_marker = self.generate_markers(
+        #     self.nodes_point, self.num_nodes_neighbor, self.edges, msg)
 
     def draw_history(self, topology):
         """Draw intersection point on odometry frame"""
@@ -93,10 +97,8 @@ class Extract_topology(Node):
 
         # target = [0, 0]
 
-
         x = target[0]*math.cos(self.robot_pose[2]) - target[1]*math.sin(self.robot_pose[2]) + self.robot_pose[0]
         y = target[0]*math.sin(self.robot_pose[2]) + target[1]*math.cos(self.robot_pose[2]) + self.robot_pose[1]
-
 
         # Append a marker point
         # p = Point()
@@ -134,6 +136,56 @@ class Extract_topology(Node):
         t4 = +1.0 - 2.0 * (y * y + z * z)
         Z = math.atan2(t3, t4)
         return Z
+
+    def generate_markers(self, nodes_point, num_nodes_neighbor, edges, msg):
+        # Add node marker
+        nodes_marker = Marker()
+        nodes_marker.header = copy.deepcopy(msg.header)
+        nodes_marker.header.frame_id = 'odom'
+        nodes_marker.ns = 'node'
+        nodes_marker.id = 0
+        nodes_marker.type = Marker.POINTS
+        nodes_marker.action = Marker.ADD
+        nodes_marker.scale.x = 0.1      # Point width
+        nodes_marker.scale.y = 0.1      # Point height
+        for i, node_point in enumerate(nodes_point):
+            p = Point(
+                x = -node_point[0],
+                y = -node_point[1],
+                z = 0.1)
+            nodes_marker.points.append(p)
+            if(num_nodes_neighbor[i] == 1):
+                nodes_marker.colors.append(copy.deepcopy(self.COLOR_LEAF))
+            elif(num_nodes_neighbor[i] > 2):
+                nodes_marker.colors.append(copy.deepcopy(self.COLOR_INTERSECTION))
+            else:
+                nodes_marker.colors.append(copy.deepcopy(self.COLOR_NODE))                
+
+        # Add edge marker
+        edges_marker = Marker()
+        edges_marker.header = copy.deepcopy(msg.header)
+        edges_marker.header.frame_id = 'odom'
+        edges_marker.ns = 'edge'
+        edges_marker.id = 0
+        edges_marker.type = Marker.LINE_LIST
+        edges_marker.action = Marker.ADD
+        edges_marker.scale.x = 0.03     # Line width
+        for edge in edges:
+            edges_marker.points.append(nodes_marker.points[edge[0]])
+            edges_marker.points.append(nodes_marker.points[edge[1]])
+            edges_marker.colors.append(copy.deepcopy(self.COLOR_EDGE))
+            edges_marker.colors.append(copy.deepcopy(self.COLOR_EDGE))
+        
+        return nodes_marker, edges_marker
+
+    def update_node_color_alpha(self, node, alpha):
+        self.nodes_marker.colors[node].a = alpha
+
+    def update_edge_color_alpha(self, edge, alpha):
+        for i, e in enumerate(self.edges):
+            if(e == edge or e[::-1] == edge):
+                self.edges_marker.colors[2*i].a = alpha
+                self.edges_marker.colors[2*i+1].a = alpha
 
 def main(args=None):
     rclpy.init(args=args)
--- src/waffle_topology/waffle_topology/graph.py
+++ src/waffle_topology/waffle_topology/graph.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 
-from geometry_msgs.msg import Point
-from std_msgs.msg import ColorRGBA
-from visualization_msgs.msg import Marker
-
 import cv2
 import numpy as np
 import math
-import copy
 
 class Node():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, id, point):
+        self.id = id
+        self.point = point
         self.neighbors = []
 
 class Graph():
-    """Topology graph from gridmap image"""
+    """Topology graph from a gridmap topology image"""
+    seq = -1
 
     def __init__(self, msg, pose):
+        Graph.seq += 1
+
         # Extract features
         img = self.convert_map_to_image(msg)
         topology = cv2.ximgproc.thinning(img)
@@ -26,34 +24,29 @@ class Graph():
         corners = cv2.goodFeaturesToTrack(topology, 20, 0.1, 6)
 
         # Search nodes and edges
-        self.nodes = list(map(lambda corner: [
+        nodes_idx = list(map(lambda corner: [
             int(corner[0][0]), int(corner[0][1])], corners))
-        self.edges = self.search_edges(
-            self.nodes,
+        edges_idx = self.search_edges(
+            nodes_idx,
             list(map(lambda contour: [contour[0][0], contour[0][1]], contours[0])))
-
+        
         # Calculate global xy coordinates of nodes
         nodes_point_local = list(map(lambda node: [
             -((node[1] * msg.info.resolution) + msg.info.origin.position.y),
-            -((node[0] * msg.info.resolution) + msg.info.origin.position.x)], self.nodes))
-        self.nodes_point = list(map(lambda point: [
+            -((node[0] * msg.info.resolution) + msg.info.origin.position.x)], nodes_idx))
+        nodes_point_global = list(map(lambda point: [
             point[0]*math.cos(pose[2]) - point[1]*math.sin(pose[2]) + pose[0],
             point[0]*math.sin(pose[2]) + point[1]*math.cos(pose[2]) + pose[1]], nodes_point_local))
 
+        # Construct nodes
+        self.nodes = []
+        for i, point in enumerate(nodes_point_global):
+            self.nodes.append(Node([Graph.seq, i], point))
+        for edge in edges_idx:
+            self.nodes[edge[0]].neighbors.append(self.nodes[edge[1]])
+
         # Analysis graph
-        self.num_nodes_neighbor = self.count_neighbors(self.nodes, self.edges)
-        self.root = self.search_closest_edge(self.nodes_point, self.edges, pose)
-
-        # Visualize graph
-        self.init_colors()
-        self.nodes_marker, self.edges_marker = self.generate_markers(
-            self.nodes_point, self.num_nodes_neighbor, self.edges, msg)
-
-    def init_colors(self):
-        self.COLOR_NODE = ColorRGBA(r=0.53, g=0.9, b=0.5, a=0.2)        # Green
-        self.COLOR_EDGE = ColorRGBA(r=0.53, g=0.9, b=0.5, a=0.2)        # Green
-        self.COLOR_INTERSECTION = ColorRGBA(r=1., g=0.73, b=0., a=0.2)  # Yellow
-        self.COLOR_LEAF = ColorRGBA(r=0.95, g=0.37, b=0.37, a=0.2)      # Red
+        self.root = self.search_closest_edge(pose)
 
     def convert_map_to_image(self, msg):
         img = np.zeros([msg.info.width, msg.info.height], np.uint8)
@@ -102,86 +95,22 @@ class Graph():
                 n_ = n
         return edges
 
-    def count_neighbors(self, nodes, edges):
-        """Return number of neighbor nodes"""
-
-        num_nodes_neighbor = [0] * len(nodes)
-        for edge in edges:
-            num_nodes_neighbor[edge[0]] += 1
-        return num_nodes_neighbor
-
-    def search_closest_edge(self, nodes_point, edges, pose):
-        """Return closest edge index from pose[x, y, theta]"""
+    def search_closest_edge(self, pose):
+        """Return closest edge[departure node, destination node] from pose"""
 
         # Search closest node
         nodes_dist = []
-        for node_point in nodes_point:
-            nodes_dist.append((node_point[0] - pose[0])**2 + (node_point[1] - pose[1])**2)
-        closest_node = nodes_dist.index(min(nodes_dist))
+        for node in self.nodes:
+            nodes_dist.append((node.point[0] - pose[0])**2 + (node.point[1] - pose[1])**2)
+        node_departure = self.nodes[nodes_dist.index(min(nodes_dist))]
 
-        # Search neighbor edges in distance
-        edges_neighbor = []
-        for edge in edges:
-            if(edge[0] == closest_node):
-                edges_neighbor.append(edge)
-        
         # Search closest edges in angle
-        edges_angle = []
-        for edge in edges_neighbor:
+        edges_abs_angle = []
+        for node in node_departure.neighbors:
             angle = math.atan2(
-                0.5 * nodes_point[edge[1]][1] - 0.5 * nodes_point[edge[0]][1],
-                0.5 * nodes_point[edge[1]][0] - 0.5 * nodes_point[edge[0]][0]
+                node.point[1] - node_departure.point[1],
+                node.point[0] - node_departure.point[0]
             ) - pose[2]
-            edges_angle.append(abs((angle + 3*math.pi) % (2*math.pi) - math.pi))
+            edges_abs_angle.append(abs((angle + 3*math.pi) % (2*math.pi) - math.pi))
         
-        return edges_neighbor[edges_angle.index(min(edges_angle))]
-
-    def generate_markers(self, nodes_point, num_nodes_neighbor, edges, msg):
-        # Add node marker
-        nodes_marker = Marker()
-        nodes_marker.header = copy.deepcopy(msg.header)
-        nodes_marker.header.frame_id = 'odom'
-        nodes_marker.ns = 'node'
-        nodes_marker.id = 0
-        nodes_marker.type = Marker.POINTS
-        nodes_marker.action = Marker.ADD
-        nodes_marker.scale.x = 0.1      # Point width
-        nodes_marker.scale.y = 0.1      # Point height
-        for i, node_point in enumerate(nodes_point):
-            p = Point(
-                x = -node_point[0],
-                y = -node_point[1],
-                z = 0.1)
-            nodes_marker.points.append(p)
-            if(num_nodes_neighbor[i] == 1):
-                nodes_marker.colors.append(copy.deepcopy(self.COLOR_LEAF))
-            elif(num_nodes_neighbor[i] > 2):
-                nodes_marker.colors.append(copy.deepcopy(self.COLOR_INTERSECTION))
-            else:
-                nodes_marker.colors.append(copy.deepcopy(self.COLOR_NODE))                
-
-        # Add edge marker
-        edges_marker = Marker()
-        edges_marker.header = copy.deepcopy(msg.header)
-        edges_marker.header.frame_id = 'odom'
-        edges_marker.ns = 'edge'
-        edges_marker.id = 0
-        edges_marker.type = Marker.LINE_LIST
-        edges_marker.action = Marker.ADD
-        edges_marker.scale.x = 0.03     # Line width
-        for edge in edges:
-            edges_marker.points.append(nodes_marker.points[edge[0]])
-            edges_marker.points.append(nodes_marker.points[edge[1]])
-            edges_marker.colors.append(copy.deepcopy(self.COLOR_EDGE))
-            edges_marker.colors.append(copy.deepcopy(self.COLOR_EDGE))
-        
-        return nodes_marker, edges_marker
-
-    def update_node_color_alpha(self, node, alpha):
-        self.nodes_marker.colors[node].a = alpha
-
-    def update_edge_color_alpha(self, edge, alpha):
-        for i, e in enumerate(self.edges):
-            if(e == edge or e[::-1] == edge):
-                self.edges_marker.colors[2*i].a = alpha
-                self.edges_marker.colors[2*i+1].a = alpha
+        return [node_departure, self.nodes[edges_abs_angle.index(min(edges_abs_angle))]]
